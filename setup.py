f6cf821e70427e1fa6a2b20bc7d50b8856d51784
Annie K. Lamar <38513786+AnnieKLamar@users.noreply.github.com>
2023-10-17 09:32:19 -0700

    New release with dependency fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'foodframe',
   packages = ['foodframe'],
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Standardize. Analyze. Impact.',
   author = 'Tushar Dalmia, Annie K. Lamar',
